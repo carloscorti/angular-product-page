0fc318ad358c2de9c07e9a0bbd6194f9454783ed
Carlos <carlosacorti@hotmail.com>
2022-04-21 21:17:39 +0200

    app.compoment: customize app.component

--- App/apm-new/src/app/app.component.ts
+++ App/apm-new/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <p>Component subline</p>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'apm-new';
+  pageTitle: string = 'New angular app :)';
 }
