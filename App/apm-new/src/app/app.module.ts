aedf10acdfd8978103510916188745c348b718f5
Carlos <carlosacorti@hotmail.com>
2022-04-29 22:57:39 +0200

    product.component: format with native/custom pipes

--- App/apm-new/src/app/app.module.ts
+++ App/apm-new/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 
+import { CurrencyCustomPipe } from "./pipes/currency.pipe";
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    CurrencyCustomPipe
   ],
   imports: [
     BrowserModule,
---
+++ App/apm-new/src/app/pipes/currency.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'customcurrency' })
+
+export class CurrencyCustomPipe implements PipeTransform {
+  transform(value: number, currencyType?: string, roundedDecimal: number = 1): string {
+    let decimalsRoundTo = roundedDecimal
+
+    if (roundedDecimal < 0) {
+      decimalsRoundTo = 0
+    }
+
+    return currencyType ? `${currencyType}${value.toFixed(decimalsRoundTo)}` : value.toFixed(decimalsRoundTo)
+  }
+}
