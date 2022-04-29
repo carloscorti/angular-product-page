d84466fac21a1ea03154e63af6bd41c24702ddb6
Carlos <carlosacorti@hotmail.com>
2022-04-29 20:47:55 +0200

    product.component: bind images properties

--- App/apm-new/src/app/products/product-list.component.ts
+++ App/apm-new/src/app/products/product-list.component.ts
@@ -1,33 +1,34 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   // styleUrls: ['./app.component.css']
 })
-
 export class ProductListComponent {
-  productListHeader: string = "Product List";
+  productListHeader: string = 'Product List';
+  productImageWidth: number = 50;
+  productImageMargin: number = 2;
   products: any[] = [
     {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
     },
     {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ]
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2021',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+  ];
 }
