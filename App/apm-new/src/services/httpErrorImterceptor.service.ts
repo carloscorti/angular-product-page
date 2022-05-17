c5f8d6ba1c8454705eba36ba59a2d8cf73de1892
Carlos Corti <carli@Carloss-MacBook-Pro.local>
2022-05-17 16:31:57 +0200

    HttpErrorInterceptor: refactor to handleError method

--- App/apm-new/src/services/httpErrorImterceptor.service.ts
+++ App/apm-new/src/services/httpErrorImterceptor.service.ts
@@ -17,22 +17,22 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      catchError((errorResponse: HttpErrorResponse) => {
-        let errorMsg: string = '';
-        if (errorResponse.error instanceof Error) {
-          // A client-side or network error occurred. Handle it accordingly.
-          errorMsg = `HttpErrorInterceptor - An error occurred: ${errorResponse.error.message}`;
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong,
-          errorMsg = `HttpErrorInterceptor - Backend returned code ${errorResponse.status}, body was: ${errorResponse.error}`;
-        }
+    return next.handle(request).pipe(catchError(this.handleError));
+  }
 
-        console.warn(errorMsg);
-        throw errorMsg;
-      })
-    );
+  handleError(errorResponse: HttpErrorResponse): Observable<HttpEvent<any>> {
+    let errorMsg: string = '';
+    if (errorResponse.error instanceof Error) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMsg = `HttpErrorInterceptor - An error occurred: ${errorResponse.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMsg = `HttpErrorInterceptor - Backend returned code ${errorResponse.status}, body was: ${errorResponse.error}`;
+    }
+
+    console.warn(errorMsg);
+    throw errorMsg;
   }
 }
 
