3bd135834926e7481c78e4ae0fa210dea1f83ef4
Carlos Corti <carli@Carloss-MacBook-Pro.local>
2022-05-25 17:51:49 +0200

    product-detail: bind template w/ product id data

--- App/apm-new/src/app/products/product-detail/product-detail.component.ts
+++ App/apm-new/src/app/products/product-detail/product-detail.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProductService } from 'src/services/data.service';
+import { IProduct } from '../product-list.component.types';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnInit {
-  pageTitle = 'Product Detail';
+export class ProductDetailComponent implements OnInit, OnDestroy {
+  pageTitle = 'Product Detail:';
+  product: IProduct | undefined;
+  productId!: number;
+  productServiceSubscription!: Subscription;
+  needErrorMessage: boolean = false;
 
-  constructor() {}
+  constructor(
+    private acivatedRoute: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.productId = Number(this.acivatedRoute.snapshot.paramMap.get('id'));
+    this.productServiceSubscription = this.productService
+      .getProductById(this.productId)
+      .subscribe({
+        next: (productData) => {
+          this.product = productData;
+          this.pageTitle = this.product
+            ? `${this.pageTitle} ${this.product.productName}`
+            : `${this.pageTitle} not found`;
+        },
+        error: (error) => {
+          console.warn('ProductDetailComponent error: ', error);
+          this.needErrorMessage = true;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.productServiceSubscription.unsubscribe();
+  }
+
+  onClickBack(): void {
+    this.router.navigate(['products']);
+  }
 }
