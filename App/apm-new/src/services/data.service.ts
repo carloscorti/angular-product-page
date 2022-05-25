75d781255bab35d82d662941ec136c9b6b8dfdbf
Carlos Corti <carli@Carloss-MacBook-Pro.local>
2022-05-25 17:49:31 +0200

    data.service: setup getProductById service

--- App/apm-new/src/services/data.service.ts
+++ App/apm-new/src/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from '../app/products/product-list.component.types';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +17,19 @@ export class ProductService {
         console.log('product list', productList);
       }),
       catchError((error) => {
-        console.warn('ProductService error: ', error);
+        console.warn('ProductService getProducts error: ', error);
+        throw error;
+      })
+    );
+  }
+
+  getProductById(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((productList) =>
+        productList.find((product) => product.productId === id)
+      ),
+      catchError((error) => {
+        console.warn('ProductService getProductById error: ', error);
         throw error;
       })
     );
